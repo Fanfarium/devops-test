1587bff0cea760116708c97ad23cf0c7c1216359
Fanfarium <artemlukianyk2005@gmail.com>
2025-08-09 09:53:56 +0000

    pipeline

--- kuber/kube.sh
+++ kuber/kube.sh
@@ -1,11 +1,11 @@
-kubectl apply -f sec-redis.yml
-kubectl apply -f dep-redis.yml
-kubectl apply -f service_redis.yml
+kubectl apply -f ./kuber/sec-redis.yml
+kubectl apply -f ./kuber/dep-redis.yml
+kubectl apply -f ./kuber/service_redis.yml
 kubectl wait --for=condition=available --timeout=6s deployment/redis
-kubectl apply -f conf-nest.yml
-kubectl apply -f dep-nest.yml
-kubectl apply -f service-nest.yml
+kubectl apply -f ./kuber/conf-nest.yml
+kubectl apply -f ./kuber/dep-nest.yml
+kubectl apply -f ./kuber/service-nest.yml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.9.1/deploy/static/provider/cloud/deploy.yaml
 kubectl wait --namespace ingress-nginx --for=condition=available --timeout=6s deployment/ingress-nginx-controller
-kubectl apply -f ingress.yml
-kubectl apply -f ingress-contr.yml
+kubectl apply -f ./kuber/ingress.yml
+kubectl apply -f ./kuber/ingress-contr.yml
